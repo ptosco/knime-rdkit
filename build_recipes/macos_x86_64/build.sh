5146aa91a9892cae56745a3011e0d9809725f42a
Paolo Tosco <paolo.tosco.mail@gmail.com>
2022-11-27 15:49:51 +0100

    added help text

--- build_recipes/macos_x86_64/build.sh
+++ build_recipes/macos_x86_64/build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env zsh
 
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+cat 1>&2 << EOF
+Usage: ./build.sh [--clean]
+Pass --clean if to do a clean build starting from scratch
+
+EOF
+    exit
+fi
 [ "$1" = "--clean" ] && ls -l | grep ^d | awk "{print \"rm -rf \"\$NF}" 1>&2 | sh
 [ ! -e ./rdkit_knime/bin/python ] && python3 -m venv rdkit_knime
 . rdkit_knime/bin/activate
